8581605e002dd0ecef1cc87707c3b6f7a128f292
bernardogomesrodrigues <giovana.giovanella@aluno.senai.br>
2026-06-09 17:22:05 -0300

    a
    a

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     return (
     <div 
       className="flex min-h-screen items-center justify-center bg-[#63783D] bg-cover bg-center bg-no-repeat px-4 font-sans"
-      style={{ backgroundImage: "url('/images/banner1.png')" }}
+      style={{ backgroundImage: "url('/images/banner3.png')" }}
     >
       
       <div className="w-full max-w-md space-y-5 rounded-2xl bg-white p-8 shadow-2xl border-2 border-[#63783D] transition-all">
@@ -38,7 +38,7 @@ export default function Login() {
               <input
                 type="email"
                 required
-                className="mt-1 w-full rounded-lg bg-[#222222] border border-neutral-700 px-4 py-3 text-white placeholder-gray-500 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-300 transition-all"
+                className="mt-1 w-full rounded-lg bg-[#222222] border border-neutral-700 px-4 py-3 text-white placeholder-gray-500 shadow-sm focus:border-[#63783D] focus:outline-none focus:ring-1 focus:ring-blue-300 transition-all"
                 placeholder="seu@email.com"
                 
               />
@@ -57,7 +57,7 @@ export default function Login() {
               <input
                 type="password"
                 required
-                className="mt-1 w-full rounded-lg bg-[#222222] border border-neutral-700 px-4 py-3 text-white placeholder-gray-500 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-300 transition-all"
+                className="mt-1 w-full rounded-lg bg-[#222222] border border-neutral-700 px-4 py-3 text-white placeholder-gray-500 shadow-sm focus:border-[#63783D] focus:outline-none focus:ring-1 focus:ring-blue-300 transition-all"
                 placeholder="••••••••"
                 
               />
@@ -74,7 +74,7 @@ export default function Login() {
           <div>
             <button
               type="submit"
-              className="w-full rounded-lg bg-[#63783D] py-3 text-sm font-bold text-white shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1 focus:ring-offset-blue-500 transition-colors uppercase tracking-wider"
+              className="w-full rounded-lg bg-[#63783D] py-3 text-sm font-bold text-white shadow-lg hover:bg-[#437a4d] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1 focus:ring-offset-blue-500 transition-colors uppercase tracking-wider"
             >
               Entrar
             </button>
