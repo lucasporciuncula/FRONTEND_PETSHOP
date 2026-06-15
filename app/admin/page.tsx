3db8bae53e79cf7eed3ebc5477c9858c43f33239
joaquim <joaquim.arend@aluno.senai.br>
2026-06-15 14:03:26 -0300

    ?
    ?

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,31 +1,47 @@
 "use client";
 
-import { useProducts } from "../hooks/useProducts";
+import { useProductContext } from "../context/ProductsContext";
 
 export default function Admin() {
   
 
-  const { products } = useProducts();
+  const {product, products, setProduct} = useProductContext()
   return (
-    <div className="flex items-center justify-center w-full h-screen">
+    <div className="flex items-center justify-center w-full h-screen flex-col col-end-1">
       <span className="tracking widest italic font-bold text-3xl text-white">
-        PÁGINA
+        PÁGINA do ADMIN
       </span>
-      <span className="tracking widest italic font-bold text-3xl text-orange-400">
-        ADMIN
-      </span>
-      <div className="flex flex-col col-end-1">
-        {products.map((prod, i) => (
-          <h1 className="font-bold flex" key={i}>
-            {prod?.label}
-            <img
-              alt={prod?.label}
-              className="w-20 h-20"
-              src={`/images/products/${prod?.animal}/${prod?.image}.jpg`}
-            />
-          </h1>
-        ))}
-      </div>
+      
+      <div className="flex flex-col gap-4 overflow-y-auto max-h-145 pr-2 flex-1">
+          {products.length === 0 ? (
+            <p className="text-center text-gray-500 my-auto py-12 font-medium">
+              Nenhum pedido encontrado neste filtro.
+            </p>
+          ) : (
+            products.map((prod) => {
+              const isSelected = product?.label === prod?.label
+              return (
+                <div
+                  key={prod?.label}
+                  onClick={() => setProduct(prod)} // Clicar em qualquer ponto do retângulo ativa o prontuário
+                  className={`p-7 rounded-4x1 border transition-all flex items-center justify-between cursor-pointer group min-h-30 ${
+                    isSelected
+                      ? "bg-[#141414] border-[#f26422] shadow-lg shadow-[#f26422]/5" // Card selecionado
+                      : "bg-[#121212] border-white/5 hover:border-white/15 hover:bg-[#151515]" // Card não selecionado
+                  }`}
+                ><div>
+                  <img className="" src={`/image/products/${prod?.animal}/${prod?.image}.jpg}`} alt={prod?.label}/>
+                  </div>
+                    <div className="flex items-center gap-12 text-sm text-gray-400">
+                        <p className="font-bold text-gray-200 mt-1 text-base">
+                          {prod?.label}
+                        </p>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
     </div>
   );
 }
--- app/context/ProductsContext.tsx
+++ app/context/ProductsContext.tsx
@@ -5,7 +5,6 @@ import {
   Dispatch,
   SetStateAction,
   useContext,
-  useEffect,
   useState,
 } from "react";
 
