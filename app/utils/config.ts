1c4835ea6cad72b30963e1b4a0a3223ff1fa2f49
joaquim <joaquim.arend@aluno.senai.br>
2026-06-25 13:42:01 -0300

    oi
    0

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = "http://10.200.80.107:6767" // comp do meio
+export const API_BASE_URL = "http://10.200.80.107:6767" // comp do meio
 //de onde tira as coisas, é o servidor
 
-export const API_BASE_URL = "http://10.200.80.132:6767" // comp do canto
+// export const API_BASE_URL = "http://10.200.80.132:6767" // comp do canto
