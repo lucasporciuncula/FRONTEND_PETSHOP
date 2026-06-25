c57c8ed71fa300d13678a58c1e18dc8cc58db84f
joaquim <joaquim.arend@aluno.senai.br>
2026-06-25 17:25:47 -0300

    front
    a

--- app/pedidos_admin/page.tsx
+++ app/pedidos_admin/page.tsx
@@ -29,35 +29,35 @@ export default function AdminPanel() {
   });
 
   return (
-    <div className="min-h-screen bg-[#121212] text-white font-sans p-8 flex flex-col gap-6 selection:bg-[#f26422]/30">
+    <div className="min-h-screen bg-[#FAFAF8] text-black font-sans p-8 flex flex-col gap-6 selection:bg-[#F5F2EC]">
       
 
-      <header className="w-full bg-[#1e1e1e] border border-white/5 rounded-3xl p-5 flex items-center justify-between shadow-xl">
+      <header className="w-full bg-[#FAFAF8] border border-[#664533] rounded-3xl p-5 flex items-center justify-between shadow-xl">
         <div className="flex items-center gap-2">
-          <div className="bg-[#664533] p-1.5 rounded-xl text-white flex items-center justify-center">
+          <div className="bg-[#FAFAF8] p-1.5 rounded-xl text-black flex items-center justify-center">
             <Flame size={28} className="fill-white" /> 
           </div>
-          <h1 className="text-2xl font-black text-white tracking-normal">
+          <h1 className="text-2xl font-black text-black tracking-normal">
             Pet<span className="text-[#664533]">Shop</span>
-            <span className="text-xs font-bold text-gray-400 uppercase tracking-widest ml-4 bg-white/5 px-3 py-1.5 rounded-xl border border-white/5">
+            <span className="text-xs font-bold text-gray-700 uppercase tracking-widest ml-4 bg-[#FAFAF8] px-3 py-1.5 rounded-xl border border-[#664533]">
               Painel Produtos - Admin
             </span>
           </h1>
         </div>
         
-        <div className="flex items-center gap-6">
-          <button className="text-xs bg-[#664533] hover:bg-[#664533] text-white font-bold uppercase tracking-wider px-5 py-3 rounded-2xl transition-all shadow-lg shadow-[#f26422]/10 active:scale-95">
+        <div className="flex items-center gap-6 ">
+          <button className="text-xs bg-[#FAFAF8] hover:bg-[#664533] text-black font-bold uppercase tracking-wider px-5 py-3 rounded-2xl transition-all shadow-lg shadow-[#f26422]/10 active:scale-95">
            Voltar para admin
           </button>
           
-          <div className="flex items-center gap-3 bg-[#121212] px-4 py-2 rounded-2xl border border-white/5">
-            <div className="text-xs text-gray-400 font-bold uppercase tracking-widest">
-              Dono: <span className="text-white">PetShop Corp</span>
+          <div className="flex items-center gap-3 bg-[#FAFAF8] px-4 py-2 rounded-2xl border border-[#664533]">
+            <div className="text-xs text-gray-700 font-bold uppercase tracking-widest">
+              Dono: <span className="text-black">PetShop Corp</span>
             </div>
-            <div className="w-px h-4 bg-white/10 mx-1" /> 
+            <div className="w-px h-4 bg-[#FAFAF8] mx-1" /> 
             <button 
               onClick={logout}
-              className="text-gray-400 hover:text-red-500 transition-colors flex items-center gap-1.5 text-xs font-bold uppercase tracking-wider cursor-pointer"
+              className="text-gray-700 hover:text-red-500 transition-colors flex items-center gap-1.5 text-xs font-bold uppercase tracking-wider cursor-pointer"
             >
               <LogOut size={20} />
               Sair
@@ -76,7 +76,7 @@ export default function AdminPanel() {
                 onClick={() => setSelectedAnimalFilter(animal)}
                 className={`px-4 py-2.5 rounded-xl transition-all ${
                   selectedAnimalFilter === animal
-                    ? "bg-[#664533] text-white shadow-md shadow-[#f26422]/10"
+                    ? "bg-[#664533] text-black shadow-md shadow-[#f26422]/10"
                     : "bg-[#121212] text-gray-400 hover:bg-[#161616] border border-white/5"
                 }`}
               >
@@ -95,7 +95,7 @@ export default function AdminPanel() {
                 onClick={() => setSelectedCategoryFilter(cat)}
                 className={`px-4 py-2.5 rounded-xl transition-all shrink-0 ${
                   selectedCategoryFilter === cat
-                    ? "bg-[#664533] text-white shadow-md shadow-[#f26422]/10"
+                    ? "bg-[#664533] text-black shadow-md shadow-[#f26422]/10"
                     : "bg-[#121212] text-gray-400 hover:bg-[#161616] border border-white/5"
                 }`}
               >
@@ -106,11 +106,11 @@ export default function AdminPanel() {
         </div>
       </section> */}
 
-      <div className="flex gap-6 items-stretch flex-1 min-h-145">      
-        <div className="flex-1 bg-[#1e1e1e] border border-white/5 rounded-4xl p-6 flex flex-col gap-6 shadow-xl">
+      <div className="flex gap-6 items-stretch flex-1 min-h-145  shadow-xl">      
+        <div className="flex-1 bg-[#FAFAF8] border border-[#664533]rounded-4xl p-6 flex flex-col gap-6 shadow-xl">
 
-          <div className="flex items-center gap-2 border-b border-white/5 pb-5 overflow-x-auto scrollbar-hide">
-            <span className="text-xs font-bold text-gray-500 uppercase tracking-wider mr-2">Filtros:</span>
+          <div className="flex items-center gap-2 border-b border-[#664533] pb-5 overflow-x-auto scrollbar-hide">
+            <span className="text-xs font-bold text-gray-800 uppercase tracking-wider mr-2">Filtros:</span>
             {[
               { id: "all", label: "Todos", count: ordersList.length },
               { id: "pending", label: "Recebidos", count: totalPending },
@@ -123,18 +123,18 @@ export default function AdminPanel() {
                 onClick={() => setSelectedStatusFilter(filter.id)} 
                 className={`px-4 py-2.5 rounded-xl text-xs font-bold transition-all shrink-0 ${
                   selectedStatusFilter === filter.id
-                    ? "bg-[#664533] text-white shadow-md shadow-[#f26422]/10" 
-                    : "bg-[#121212] text-gray-400 hover:bg-[#161616] border border-white/5" 
+                    ? "bg-[#FAFAF8] text-black shadow-md shadow-[#f26422]/10" 
+                    : "bg-[#FAFAF8] text-gray-700 hover:bg-[#664533] border border-[#664533]" 
                 }`}
               >
-                {filter.label} <span className="opacity-50 ml-1 bg-white/5 px-1.5 py-0.5 rounded-md">{filter.count}</span>
+                {filter.label} <span className="opacity-50 ml-1 bg-[#FAFAF8] px-1.5 py-0.5 rounded-md">{filter.count}</span>
               </button>
             ))}
           </div>
 
-          <div className="flex flex-col gap-4 overflow-y-auto max-h-145 pr-2 flex-1">
+          <div className="flex flex-col gap-4 overflow-y-auto max-h-145 pr-2 flex-1  shadow-xl">
             {filteredOrders.length === 0 ? (
-              <p className="text-center text-gray-500 my-auto py-12 font-medium">Nenhum pedido encontrado neste filtro.</p>
+              <p className="text-center text-gray-800 my-auto py-12 font-medium">Nenhum pedido encontrado neste filtro.</p>
             ) : (
               filteredOrders.map((order) => {
                 const statusMeta = ORDER_STATUSES[order.status as keyof typeof ORDER_STATUSES];
@@ -146,27 +146,27 @@ export default function AdminPanel() {
                     onClick={() => setSelectedOrder(order)} 
                     className={`p-7 rounded-4xl border transition-all flex items-center justify-between cursor-pointer group min-h-30 ${
                       isSelected
-                        ? "bg-[#141414] border-[#664533] shadow-lg shadow-[#f26422]/5" 
-                        : "bg-[#121212] border-white/5 hover:border-white/15 hover:bg-[#151515]" 
+                        ? "bg-[#FAFAF8] border-[#664533] shadow-lg shadow-[#f26422]/5" 
+                        : "bg-[#FAFAF8] border-[#664533] hover:border-[#664533] hover:bg-[#664533]" 
                     }`}
                   >
                     <div className="flex flex-col gap-4">
                       <div className="flex items-center gap-3.5">
                         <span className="text-3xl font-black tracking-tight">{order.id}</span>
-                        <span className="bg-[#1e1e1e] px-3 py-1 rounded-xl border border-white/5 text-[10px] font-black uppercase tracking-wider text-gray-300 flex items-center gap-1.5">
+                        <span className="bg-[#FAFAF8] px-3 py-1 rounded-xl border border-[#664533]text-[10px] font-black uppercase tracking-wider text-gray-700 flex items-center gap-1.5">
                           {statusMeta?.icon} {statusMeta?.label}
                         </span>
                       </div>
 
-                      <div className="flex items-center gap-12 text-sm text-gray-400">
+                      <div className="flex items-center gap-12 text-sm text-gray-800  shadow-xl">
                         <div>
-                          <p className="text-[10px] uppercase font-bold text-gray-600 tracking-wider">Cliente</p>
-                          <p className="font-bold text-gray-200 mt-1 text-base">{order.customerName}</p>
+                          <p className="text-[10px] uppercase font-bold text-gray-800 tracking-wider">Cliente</p>
+                          <p className="font-bold text-gray-600 mt-1 text-base">{order.customerName}</p>
                         </div>
                         <div>
-                          <p className="text-[10px] uppercase font-bold text-gray-600 tracking-wider">Horário</p>
-                          <p className="font-bold text-gray-200 mt-1 flex items-center gap-1">
-                            <Clock size={14} className="text-gray-500" />
+                          <p className="text-[10px] uppercase font-bold text-gray-800 tracking-wider">Horário</p>
+                          <p className="font-bold text-gray-600 mt-1 flex items-center gap-1">
+                            <Clock size={14} className="text-gray-800" />
                             {order.statusHistory[0]?.time || "13:34"} 
                           </p>
                         </div>
@@ -174,8 +174,8 @@ export default function AdminPanel() {
                     </div>
 
                     <div className="text-right pr-2">
-                      <p className="text-[10px] uppercase font-bold text-gray-600 tracking-wider">Total</p>
-                      <p className="text-2xl font-black text-white mt-1 group-hover:text-[#ccbcbcd2] transition-colors">
+                      <p className="text-[10px] uppercase font-bold text-gray-800 tracking-wider  shadow-xl">Total</p>
+                      <p className="text-2xl font-black text-black mt-1 group-hover:text-[#ccbcbcd2] transition-colors">
                         <span className="text-[#664533] text-sm font-bold mr-0.5">R$</span>
                         {order.total.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
                       </p>
@@ -187,21 +187,21 @@ export default function AdminPanel() {
           </div>
         </div>
 
-        <div className="w-full max-w-sm bg-[#1e1e1e] border border-white/5 rounded-4xl p-6 flex flex-col gap-5 shadow-xl sticky top-6">
+        <div className="w-full max-w-sm bg-[#FAFAF8] border border-[#664533] rounded-4xl p-6 flex flex-col gap-5 shadow-xl sticky top-6">
           {selectedOrder ? (
             <>
-              <div className="flex justify-between items-center border-b border-white/5 pb-4">
+              <div className="flex justify-between items-center border-b border-[#664533]pb-4">
                 <h3 className="text-sm font-black uppercase tracking-wider">Prontuário do Pedido</h3>
                 <button 
                   onClick={() => setSelectedOrder(null)} 
-                  className="p-1.5 hover:bg-white/5 rounded-xl text-gray-400 hover:text-white transition"
+                  className="p-1.5 hover:bg-white/5 rounded-xl text-black hover:text-black transition"
                 >
                   <X size={16} />
                 </button>
               </div>
 
-              <div className="flex flex-col gap-1 bg-[#121212] border border-white/5 p-4 rounded-2xl">
-                <span className="text-[10px] font-bold text-gray-600 uppercase tracking-widest">Protocolo</span>
+              <div className="flex flex-col gap-1 bg-[#FAFAF8] border border-[#664533] p-4 rounded-2xl shadow-xl">
+                <span className="text-[10px] font-bold text-black uppercase tracking-widest">Protocolo</span>
                 <div className="flex justify-between items-center">
                   <span className="text-xl font-black">{selectedOrder.id}</span>
                   <span className="text-xs font-bold text-green-400">
@@ -210,31 +210,31 @@ export default function AdminPanel() {
                 </div>
               </div>
 
-              <div className="flex flex-col gap-3">
-                <h4 className="text-[10px] font-black uppercase text-gray-500 tracking-wider">Informações do Cliente</h4>
-                <div className="flex flex-col gap-3 text-xs font-medium text-gray-300">
+              <div className="flex flex-col gap-3  shadow-xl">
+                <h4 className="text-[10px] font-black uppercase text-black tracking-wider">Informações do Cliente</h4>
+                <div className="flex flex-col gap-3 text-xs font-medium text-black">
                   <div className="flex items-center gap-2.5">
-                    <UserIcon size={15} className="text-gray-500" />
-                    <span className="text-gray-200 font-bold">{selectedOrder.customerName}</span>
+                    <UserIcon size={15} className="text-black" />
+                    <span className="text-black font-bold">{selectedOrder.customerName}</span>
                   </div>
                   <div className="flex items-center gap-2.5">
-                    <Phone size={15} className="text-gray-500" />
+                    <Phone size={15} className="text-black" />
                     <span>{selectedOrder.customerPhone}</span>
                   </div>
                   <div className="flex items-start gap-2.5">
-                    <MapPin size={15} className="text-gray-500 mt-0.5 shrink-0" />
+                    <MapPin size={15} className="text-black mt-0.5 shrink-0" />
                     <span className="leading-relaxed">{selectedOrder.address}</span>
                   </div>
                 </div>
               </div>
 
-              <div className="flex flex-col gap-2 border-t border-b border-white/5 py-4 my-1">
-                <h4 className="text-[10px] font-black uppercase text-gray-500 tracking-wider mb-1">Produtos comprados</h4>
+              <div className="flex flex-col gap-2 border-t border-b border-[#664533] py-4 my-1  shadow-xl">
+                <h4 className="text-[10px] font-black uppercase text-black tracking-wider mb-1">Produtos comprados</h4>
                 <div className="flex flex-col gap-2.5 max-h-40 overflow-y-auto pr-1">
                   {selectedOrder.items.map((item, idx) => (
                     <div key={idx} className="flex justify-between text-xs font-bold">
-                      <span className="text-gray-400">
-                        {item.quantity}x <span className="text-white font-medium">{item.name}</span>
+                      <span className="text-black">
+                        {item.quantity}x <span className="text-black font-medium">{item.name}</span>
                       </span>
                       <span>
                         R$ {(item.price * item.quantity).toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
@@ -250,16 +250,16 @@ export default function AdminPanel() {
                 </div>
               )}
 
-              <div className="mt-auto flex flex-col gap-2 bg-[#121212] border border-white/5 p-4 rounded-2xl text-xs font-medium text-gray-400">
+              <div className="mt-auto flex flex-col gap-2 bg-[#FAFAF8] border border-[#664533] p-4 rounded-2xl text-xs font-medium text-black">
                 <div className="flex justify-between">
                   <span>Subtotal:</span>
-                  <span className="text-gray-200">R$ {selectedOrder.subtotal.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</span>
+                  <span className="text-black">R$ {selectedOrder.subtotal.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</span>
                 </div>
                 <div className="flex justify-between">
                   <span>Taxa de Entrega:</span>
-                  <span className="text-gray-200">R$ {selectedOrder.deliveryFee.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</span>
+                  <span className="text-black">R$ {selectedOrder.deliveryFee.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</span>
                 </div>
-                <div className="flex justify-between items-center border-t border-white/5 pt-2 mt-1 text-white font-black text-sm">
+                <div className="flex justify-between items-center border-t border-[#664533] pt-2 mt-1 text-black font-black text-sm">
                   <span>Total Geral:</span>
                   <span className="text-[#664533] text-xl">
                     R$ {selectedOrder.total.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
@@ -268,7 +268,7 @@ export default function AdminPanel() {
               </div>
             </>
           ) : (
-            <div className="text-center text-gray-500 my-auto text-xs font-medium">
+            <div className="text-center text-gray-500 my-auto text-xs font-medium  shadow-xl">
               Selecione um pedido na lista para visualizar o prontuário aqui.
             </div>
           )}
